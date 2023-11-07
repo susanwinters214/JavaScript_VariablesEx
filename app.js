f2873c0a7ba4d00272c10c129f2d4f71d6dbb2dd
Susan Winters <susanwinters214@gmail.com>
2023-11-06 20:44:37 -0600

    changed file name and added some styles

---
+++ app.js
@@ -0,0 +1,24 @@
+
+var age = 13;
+
+const firstName = "Susan";
+let lastName = "Winters";
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+alert("Hello!")
